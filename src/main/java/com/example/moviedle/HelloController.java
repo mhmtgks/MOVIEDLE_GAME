a90a15e6113ce4c0406ea3e8ea02d053affcd0bf
Mehmet Göksu <goksumehmetgs@gmail.com>
2023-06-21 17:11:29 +0300

    optimized controller

--- src/main/java/com/example/moviedle/HelloController.java
+++ src/main/java/com/example/moviedle/HelloController.java
@@ -29,48 +29,23 @@ public class HelloController {
         if(click==1){
         entered=dosyaceker.finder(txt.getText());
         h1.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-            kontrolcü(l1, 0);
-            kontrolcü(l2, 1);
-            kontrolcü(l3, 2);
-            kontrolcü(l4, 3);
-            kontrolcü(l5, 4);
-            kontrolcü(l6, 5);
+            kontrolcü(l1,l2,l3,l4,l5,l6);
         }else if(click==2){
             hboxborderchange(h1,h2);
             entered=dosyaceker.finder(txt.getText());
-            kontrolcü(l21, 0);
-            kontrolcü(l22, 1);
-            kontrolcü(l23, 2);
-            kontrolcü(l24, 3);
-            kontrolcü(l25, 4);
-            kontrolcü(l26, 5);
+            kontrolcü(l21,l22,l23,l24,l25,l26);
         }else if(click==3){
             hboxborderchange(h2,h3);
             entered=dosyaceker.finder(txt.getText());
-            kontrolcü(l31, 0);
-            kontrolcü(l32, 1);
-            kontrolcü(l33, 2);
-            kontrolcü(l34, 3);
-            kontrolcü(l35, 4);
-            kontrolcü(l36, 5);
+            kontrolcü(l31,l32,l33,l34,l35,l36);
         }else if(click==4){
             hboxborderchange(h3,h4);
             entered=dosyaceker.finder(txt.getText());
-            kontrolcü(l41, 0);
-            kontrolcü(l42, 1);
-            kontrolcü(l43, 2);
-            kontrolcü(l44, 3);
-            kontrolcü(l45, 4);
-            kontrolcü(l46, 5);
+            kontrolcü(l41,l42,l43,l44,l45,l46);
         }else if(click==5){
             hboxborderchange(h4,h5);
             entered=dosyaceker.finder(txt.getText());
-            kontrolcü(l51, 0);
-            kontrolcü(l52, 1);
-            kontrolcü(l53, 2);
-            kontrolcü(l54, 3);
-            kontrolcü(l55, 4);
-            kontrolcü(l56, 5);
+            kontrolcü(l51,l52,l53,l54,l55,l56);
         }
         if((click==5)&&entered!=chosen)                         // Losing Situation
         {
@@ -102,17 +77,6 @@ public class HelloController {
                 stage.close();
                 alert.close();
             }
-           /*
-            Platform.runLater( () -> {
-                try {
-                   // Platform.exit();
-                    new HelloApplication().start( new Stage() );
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });*/
-
-
         }else if((click<=5)&&(entered==chosen))                  //Winning Situation
         {
             Alert alert = new Alert(Alert.AlertType.NONE);
@@ -151,12 +115,14 @@ public class HelloController {
     public void setredd(Label lbl){             // to set labels red
         lbl.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
     }
-    public void kontrolcü(Label label,int  bit){
-        label.setText(dosyaceker.getEleman().get(entered+bit));
-        if(label.getText().equals(dosyaceker.getEleman().get(chosen+bit)))
-            setgreenn(label);
+    public void kontrolcü(Label label1,Label label2,Label label3,Label label4,Label label5,Label label6){
+        Label ll[]= {label1,label2,label3,label4,label5,label6};
+        for(int i=0;i<6;i++){
+        ll[i].setText(dosyaceker.getEleman().get(entered+i));
+        if(ll[i].getText().equals(dosyaceker.getEleman().get(chosen+i)))
+            setgreenn(ll[i]);
         else
-            setredd(label);}
+            setredd(ll[i]);}}
     public void hboxborderchange(HBox eski,HBox yeni){
         eski.setBorder(new Border(new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.EMPTY)));
         yeni.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));  }
